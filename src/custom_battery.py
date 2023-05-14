7ba1bc1abceedace5d38e81e24ebb202a0b0e701
Ben Carlson <bcarlson336@gmail.com>
2023-05-14 01:44:24 -0600

    Added documentation to CustomBattery class

--- src/custom_battery.py
+++ src/custom_battery.py
@@ -2,37 +2,71 @@ import dronekit as dk
 from typing import Dict, List
 import numpy as np
 import random
-# HERELINK_TELEM
+
 
 class CustomBattery(dk.Battery):
+    '''
+    Custom Battery class that extends the dronekit Battery class
+    '''
     
     def __init__(self, battery : dk.Battery, battery_cap_mah : int):
+        '''
+        Initialize the CustomBattery object
+
+        :param battery: Battery object with the initial battery information
+        :type: dk.Battery
+        :param battery_cap_mah: Initial battery capacity in mAh (used unless otherwise updated)
+        :type: int
+        '''
+
         super().__init__(battery.voltage, battery.current, battery.level)
 
-        # Watts * 3600 seconds/hour
-        self.capacity_J = self.voltage * battery_cap_mah * 3600
+        self.capacity_J = self._joules(battery.voltage, battery_cap_mah)
         self._max_cap_j = 0
         
         self._all_data_lin_reg_params : Dict[str, float] = {}
         self.pairs_lin_reg_params : List[Dict[str, float]] = []
 
     def update_cap_mah(self, battery_cap_mah : int):
-        # Watts * 3600 seconds/hour
+        '''
+        Update the battery capacity in mAh (coverted to Joules)
+
+        :param battery_cap_mah: New battery capacity in mAh
+        :type: int
+        '''
         print(f"Updating battery capacity to {battery_cap_mah} mAh")
-        self.capacity_J = self.voltage * battery_cap_mah * 3.6
+        self.capacity_J = self._joules(self.voltage, battery_cap_mah)
 
     def update_cap_j(self, battery_cap_j : float):
-        #print(f"Updating battery capacity to {battery_cap_j} J")
+        '''
+        Update the battery capacity in Joules
+
+        :param battery_cap_j: New battery capacity in Joules
+        :type: float
+        '''
+
         self.capacity_J = battery_cap_j
         self._max_cap_j = max(self._max_cap_j, self.capacity_J)
 
     def get_capacity_percentage(self) -> float:
+        '''
+        Get the battery capacity as a percentage of the maximum capacity
+        '''
         return self.capacity_J / self._max_cap_j
 
     # Get Joules/s consumption based on the CPU utilization -> Energy consumption model
     # 1 Watt = 1 Joule/second
     def get_js_for_util(self, cpu_utilization : float, pair_idx) -> float:
-        #print(f"Fetching J/s for {cpu_utilization:.2f}% utilization")
+        '''
+        Get the Joules/second (Wattage) consumption for a given CPU utilization and pair (to index regression parameters)
+
+        :param cpu_utilization: CPU utilization to get the Joules/second consumption for
+        :type: float
+        :param pair_idx: Index of the pair to get the regression parameters for
+        :type: int
+        :return: Joules/second consumption
+        :rtype: float
+        '''
         try:
             curr_lin_reg = self.pairs_lin_reg_params[pair_idx]
         except IndexError:
@@ -58,6 +92,12 @@ class CustomBattery(dk.Battery):
             return 0
 
     def update(self, battery : dk.Battery):
+        '''
+        Update the battery information from the dk.Battery object
+
+        :param battery: Updated battery object
+        :type: dk.Battery
+        '''
         self.voltage = battery.voltage
         self.current = battery.current
         self.level = battery.level
@@ -65,3 +105,16 @@ class CustomBattery(dk.Battery):
 
     def __str__(self):
         return f"Custom {super().__str__()},Capacity={self.capacity_J} J"
+    
+    def _joules(self, volt : float, mah : int) -> float:
+        '''
+        Convert voltage and mAh to Joules
+
+        :param volt: Voltage
+        :type: float
+        :param mah: mAh
+        :type: int
+        :return: Joules
+        :rtype: float
+        '''
+        return volt * mah * 3.6
